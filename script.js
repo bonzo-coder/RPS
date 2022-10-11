dff1df33c1ec0a00c866edc50cd22975d8cd02af
bonzo-coder <bonzo.code.maker@gmail.com>
2022-10-11 22:46:23 +0200

    index.html

--- script.js
+++ script.js
@@ -1,70 +1,123 @@
-        let userWeaponType="";
-        let choose = ["Paper", "Rock", "Scissors"];
+        let btnRock='';
+        let btnPaper='';
+        let btnScissors='';
         let computerChoice;
         let newText;
         let gameMessage="";
         let userPoints=0;
         let compPoints=0;
         let i=0;
+        const arr = ['Rock','Paper','Scissors'];
+        let button;
+        let j=0;
 
         function getComputerChoice() { 
-            computerChoice = choose[Math.floor(Math.random() * choose.length)];
+            computerChoice = arr[Math.floor(Math.random() * arr.length)];
             console.log(computerChoice);
             return computerChoice;
             }
-
-        function userChoice () {
-            userWeaponType = document.getElementById("inputBlock").value;
-            newText = (userWeaponType.toUpperCase()).slice(0,1) + (userWeaponType.toLowerCase()).slice(1,10);
-            console.log(newText);
-            return newText;
-            }        
-
-        function message(text) {
-            document.getElementById("gameMessage").innerHTML=text;
-            }
             
-        function clean () {
-            document.getElementById("inputBlock").value="";
-            }
-        
+        function userChoice () {
+            document.getElementById("bRock").addEventListener('click', function() {
+                newText= btnRock;
+                return newText;
+            });
+            document.getElementById("bPaper").addEventListener('click', function() {
+                newText= btnPaper;
+                return newText;
+            });
+            document.getElementById("bScissors").addEventListener('click', function() {
+                newText= btnScissors;
+                return newText;
+            });
+            button.classList.remove('playing');
+        }
+
         function clickButton () {
-            document.getElementById("inputBlock").value="Type your choice here";
-            document.getElementById("playButton").addEventListener("click", playRound);
+            document.getElementById("playButton").addEventListener("click", gameStart);
             }
            
-        function playRound () {
-            getComputerChoice();
+        function gameStart () {
+            document.getElementById('playButton').remove('button');
+            let div = document.createElement('div');
+            div.id="choiceButton";
+            for (j; j<arr.length;j++){
+                button = document.createElement('button');
+                button.type='button';
+                button.id="b"+arr[j];
+                button.textContent = arr[j];
+                button.classList.add('key');
+                document.getElementById('btnBox').appendChild(div);
+                document.getElementById('choiceButton').appendChild(button);
+            }
+            btnRock = document.getElementById("bRock").value="Rock";
+            btnPaper = document.getElementById("bPaper").value="Paper";
+            btnScissors = document.getElementById("bScissors").value="Scissors";
             userChoice();
+            document.getElementById("choiceButton").addEventListener('click', playRound);
+                   
+        }  
+
+        function transition () {
+            if (newText=='Rock'){
+                let elementHighlighted = document.getElementById("bRock");
+                elementHighlighted.classList.add("playing");
+            }
+            else if (newText=='Paper'){
+                let elementHighlighted = document.getElementById("bPaper");
+                elementHighlighted.classList.add("playing");
+            }
+            else if (newText=='Scissors'){
+                let elementHighlighted = document.getElementById("bScissors");
+                elementHighlighted.classList.add("playing");
+            }
+        }
+
+        function removeTransition(e) {
+            if (e.propertyName !== 'transform') return;
+            e.target.classList.remove('playing');
+          }     
+            
+        function message(text) {
+            document.getElementById("gameMessage").innerHTML=text;
+        }
+
+        function playRound () {
+            userChoice();
+            transition();
+            const keys = Array.from(document.querySelectorAll('.key'));
+            keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+            getComputerChoice();
             if (computerChoice=="Paper") {
-                if (newText=="Paper"){message("DRAW"); i=i-1;}
+                if (newText=="Paper"){message("DRAW"); i=i;}
                 else if (newText=="Scissors") {message("You Win! Scissors beats Paper");userPoints=userPoints+1;}
                 else if (newText=="Rock"){message("You Lose! Paper beats Rock");compPoints=compPoints+1;}
                 else { message("Start again. Follow instructions");i=i-1;}}
             else if (computerChoice=="Scissors") {
                 if (newText=="Paper"){message("You Lose! Scissors beats Paper");compPoints=compPoints+1;}
-                else if (newText=="Scissors") {message("DRAW"); i=i-1;}
+                else if (newText=="Scissors") {message("DRAW"); i=i;}
                 else if (newText=="Rock"){message("You Win! Rock beats Scissors");userPoints=userPoints+1;}
                 else { message("Start again. Follow instructions");i=i-1;}}
             else   {
                 if (newText=="Paper"){message("You Win! Paper beats Rock");userPoints=userPoints+1;}
                 else if (newText=="Scissors") {message("You Lose! Rock beats Scissors");compPoints=compPoints+1;}
-                else if (newText=="Rock"){message("DRAW"); i=i-1;}
+                else if (newText=="Rock"){message("DRAW"); i=i;}
                 else { message("Start again. Follow instructions");i=i-1;}
             }
-            clean ();
-            game();
+            
+            newText='';
+            scoreOfGame();
+            return i;
         }
         
-        
-        function game() {
-            i = i + 1;
-            if (i<6) {
-            clickButton();
+        function scoreOfGame() {
+            i=i+1;
+            if (compPoints <5 && userPoints<5) {
+
             }
             else {
-                document.getElementById("inputBlock").style.visibility="hidden";
-                document.getElementById("playButton").style.visibility="hidden";
+                document.getElementById("choiceButton").style.visibility="hidden";
+                
                 if (userPoints>compPoints){
                 finalScore.textContent="YOU WON that GAME"; 
                 }
@@ -76,13 +129,12 @@
                 }
                 document.getElementById("resetButton").style.display="block";
             }
-            console.log(userPoints);
-            console.log(compPoints);
+            
             document.getElementById("gameScore").value="Current game state after" + i +"games";
             document.getElementById("gameScore").value="Current game state after" + i +"games";
-            gameScore.textContent="Current game state after " + (i-1) +" games";
+            gameScore.textContent="Current game state after " + (i) +" games";
             userScore.textContent="Player: "+userPoints +"    Computer "+compPoints;
         }
 
           
-        game();
+        clickButton();
